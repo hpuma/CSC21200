1b6b64427f1e790401b1c6b684c9673b6fe8e8a8
Henry_Puma <pumahenry@live.com>
2019-03-26 17:14:12 -0400

    cpp_update

--- CSC21200/HW4/hw4q2.cpp
+++ CSC21200/HW4/hw4q2.cpp
@@ -1,11 +1,14 @@
 #include "hw4q2.h"
+#ifndef __HW4Q2_H__
+#define __HW4Q2_H__
 template<class Item>
 Queue<Item>::Queue(){
     data = new Item[DEF_CAP];
+    capacity = DEF_CAP;
     first = 0;
     last = 0;
     used = 0;
-    capacity = DEF_CAP;
+    
 }
 
 template<class Item>
@@ -19,16 +22,26 @@ capacity = source.capacity;
 
 template<class Item>
 Queue<Item>::~Queue(){
-
+    delete data;
 }
 
 template<class Item>
 void Queue<Item>::push(const Item& entry){
+    if(!(used > capacity)){
+
+
+    }
+
 
 }
 template<class Item>
 void Queue<Item>::pop(){
+    if(!empty()){
+        first++;
+    }
+    
 }
+
 
 template<class Item>
 void Queue<Item>::operator=(const Queue<Item>& source){
@@ -37,14 +50,21 @@ void Queue<Item>::operator=(const Queue<Item>& source){
 
 template<class Item>
 size_t Queue<Item>::size() const{
+    return used;
 
 }
 template<class Item>
 bool Queue<Item>::empty() const{
+    return (used == 0);
 
 }
 template<class Item>
 Item Queue<Item>::front() const{
+    if(!empty()){
+        return data[first];
+    }
+    return null;
+    
 
 }
-
+#endif
