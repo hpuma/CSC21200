d913c81729b7261ea7f1e6d0da20c4bd44dea63a
Henry_Puma <pumahenry@live.com>
2019-03-27 14:58:11 -0400

    beginning Q5

---
+++ CSC21200/HW4/hw4q5.cpp
@@ -0,0 +1,25 @@
+#include "hw4q5.h"
+#ifndef __HW4Q4_CPP__
+#define __HW4Q4_CPP__
+
+template <class Item>
+Queue2Stack<Item>::Queue2Stack(){}
+
+template <class Item>
+void Queue2Stack<Item>::push(const Item& entry){}
+
+template <class Item>
+void Queue2Stack<Item>::pop(){}
+
+template <class Item>
+size_t Queue2Stack<Item>::size() const{return 0;}
+
+template <class Item>
+bool Queue2Stack<Item>::empty() const{return false;}
+
+template <class Item>
+Item Queue2Stack<Item>::top() const{
+	return Item();
+}
+#endif
+
