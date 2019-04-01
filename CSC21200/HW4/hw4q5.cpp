3dd04dc5466f416e5537447abc4ca1ee22df0554
Henry_Puma <pumahenry@live.com>
2019-03-31 21:09:55 -0400

    working_files

--- CSC21200/HW4/hw4q5.cpp
+++ CSC21200/HW4/hw4q5.cpp
@@ -9,35 +9,21 @@ Queue2Stack<Item>::Queue2Stack(){
 
 template <class Item>
 void Queue2Stack<Item>::push(const Item& entry){
-	if(!(q2.empty())){
+	while(!q1.empty()){
+		q2.push(q1.front());
+		q1.pop();
+	}
+	q1.push(entry);
+	while(!q2.empty()){
 		q1.push(q2.front());
 		q2.pop();
-		q2.push(entry);
-		used = q1.size()+ q2.size();
 	}
-	else{
-		q2.push(entry);
-		used = q1.size() + q2.size();
-	}
+	used++;
 }
-
 template <class Item>
 void Queue2Stack<Item>::pop(){
-	if(!(q2.empty())){
-		q2.pop();
-		used--;
-		size_t tempSize = q1.size();
-		for(size_t i = 1 ;  i <= tempSize ; i++ ){
-			q2.push(q1.front());
-			q1.pop();
-		}
-		size_t secondStack  = q2.size() - 1;
-		for(size_t j = 1 ; j <= secondStack; j++){
-			q1.push(q2.front());
-			q2.pop();
-		}
-	}
-			
+	q1.pop();
+	used--;			
 }
 
 template <class Item>
@@ -47,12 +33,12 @@ size_t Queue2Stack<Item>::size() const{
 
 template <class Item>
 bool Queue2Stack<Item>::empty() const{
-	return ((q1.empty()) && (q2.empty()));
+	return (q1.empty());
 }
 
 template <class Item>
 Item Queue2Stack<Item>::top() const{
-	return q2.front();
+	return (q1.front());
 }
 #endif
 
