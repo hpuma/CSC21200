9e81072fbeaf2388e62ab452c2309a6a4c57f6d1
Henry_Puma <pumahenry@live.com>
2019-04-09 02:21:20 -0400

    changed_names_edited

---
+++ CSC21200/HW5/btNode.cpp
@@ -0,0 +1,40 @@
+#ifndef __BTNODE_CPP__
+#define __BTNODE_CPP__
+#include "btNode.h"
+
+template <class Process, class Item>
+void btNode<Process, Item>::inorder(Process f, btNode<Item>* nodePtr){
+
+} 
+
+template <class Process, class Item>
+void btNode<Process, Item>::preorder(Process f, btNode<Item>* nodePtr){
+
+}
+
+template <class Process, class Item>
+void btNode<Process, Item>::postorder(Process f, btNode<Item>* nodePtr){
+
+}
+
+template <class Item, class SizeType>
+void btNode<Item, SizeType>::print(btNode<Item>* nodePtr, SizeType depth){
+
+}
+
+template <class Item>
+void btNode<Item>::clearTree(btNode<Item>*& rootPtr){
+
+}
+
+template <class Item>
+btNode<Item>* btNode<Item>::copyTree(const btNode<Item>* rootPtr){
+    
+}
+
+template <class Item>
+size_t btNode<Item>::treeSize(const btNode<Item>* nodePtr){
+
+}
+
+#endif
