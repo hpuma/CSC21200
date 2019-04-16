4cbb327359361b6147b73af786849220210dd032
hpuma <pumahenry@live.com>
2019-04-16 14:12:45 -0400

    add_comments_&&_defined_remove

--- CSC21200/HW5/hw5q4.cpp
+++ CSC21200/HW5/hw5q4.cpp
@@ -10,42 +10,79 @@ BST<Item>::BST(){
 //FROM BOOK, PAGE 553
 template <class Item>
 void BST<Item>::insert(const Item& entry){
-	if(tree.size() == 0 ){
-		tree.createFirstNode(entry);
+	if(tree.size() == 0 ){// When the tree is emptry.
+		tree.createFirstNode(entry); // The tree adds the entry as its root node.
 	}
 	else{
-		tree.shiftToRoot();
-		btNode<Item>* curr = tree.getNode();
-		btNode<Item>* newNode = new btNode<Item>(entry);
-		bool done = false;
+		tree.shiftToRoot(); // Move the tree pointer to the root.
+		btNode<Item>* curr = tree.getNode(); // Set a pointer to the root node;
+		btNode<Item>* newNode = new btNode<Item>(entry); // Create a node with entry.
+		bool done = false; // Boolean determining if the node has been inserted.
 
-		while(!done){
-			if(curr->getData() >= entry){
-				if(curr->getLeft() == NULL){
-					curr->setLeft(newNode);
-					done = true;
+		while(!done){ // While the node has not been inserted.
+
+			if(curr->getData() >= entry){  // If entry is less than the current node.
+				if(curr->getLeft() == NULL){ // If the left node is empty.
+					curr->setLeft(newNode); // Add the new node with the entry.
+					done = true;	// Node has been added .. done = true.
 				}
-				else if(curr->getLeft() != NULL){
-					curr = curr->getLeft();
-				}
-			}
-			if(curr->getData() <= entry){
-				if(curr->getRight() == NULL){
-					curr->setRight(newNode);
-					done = true;
-				}
-				else if(curr->getRight() != NULL){
-					curr = curr->getRight();
+				else if(curr->getLeft() != NULL){ //If there is a left node.
+					curr = curr->getLeft();	// Keep going left.
 				}
 			}
-			
+
+			if(curr->getData() <= entry){ // If entry is greater than the current node.
+				if(curr->getRight() == NULL){ // If the right node is empty.
+					curr->setRight(newNode); // Add the newNode.
+					done = true;	// Node has been added, set done = true.
+				}
+				else if(curr->getRight() != NULL){ // If there is a right node already.
+					curr = curr->getRight(); // Keep going left.
+				}
+			}
 		}
 	}
-	count++;
+	count++; // Node will always be added
 }
-
+// On Bottom of page 555
 template <class Item>
 void BST<Item>::remove(const Item& target){
+	tree.shiftToRoot();
+	if(tree.retrieve() == NULL){
+		return;
+	}
+	
+	if(tree.retrieve() > target){
+		tree.shiftLeft();
+		remove(target);
+	}
+	if(tree.retrieve() < target){
+		tree.shiftRight();
+		remove(target);
+	}
+
+	if(tree.retrieve() == target){
+		btNode<Item> deleteMe;
+		if(!tree.hasLeft()){
+			deleteMe = tree.getNode();
+			transplant(tree.getNode(),tree.getRight());
+			delete deleteMe;
+		}
+		else{
+			if(tree.getRight() == NULL){
+				deleteMe = tree.Node();
+				tree.setRoot(tree.getLeft());
+				delete deleteMe;
+			}
+			else if(tree.getRight() != NULL){
+				btNode<Item>* curr = tree.getNode();
+				while(!curr->isLeaf()){
+					curr = curr->getRight();
+				}
+				delete curr;
+			}
+		}
+	}
 
 }
 
