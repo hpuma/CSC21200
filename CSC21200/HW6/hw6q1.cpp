78c23515bb08b8c50df9f427e32a250a3f9810e3
Henry_Puma <pumahenry@live.com>
2019-05-10 10:52:41 -0400

    FINAL_WITH_COMMENTS

--- CSC21200/HW6/hw6q1.cpp
+++ CSC21200/HW6/hw6q1.cpp
@@ -6,12 +6,15 @@
 template <class Item>
 graphM<Item>::graphM(){
     capacity = DEF_CAP;
-    label = new Item[capacity]; //? default value.. or only for int?
-    matrix = new int*[capacity]; // Rows of the matrix.
-    for (size_t i = 0; i < capacity; i++){ // Columns of Matrix
+    // NOTE label Array contains the vertexs.
+    label = new Item[capacity](); // Initialize an array with default values of the type ... Default value for any type.. or only for int?
+    // NOTE: matrix  Array contains the intersection of vertexs, basically which two vertex connect.
+    matrix = new int*[capacity]; // Columns of matrix.
+    for (size_t i = 0; i < capacity; i++){ // Rows of matrix.
         matrix[i] = new int[capacity];
     } 
-    count = 0;
+    //NOTE: count keeps track of the number of vertices.
+    count = 0; 
 }
 
 //COPY CONSTRUCTOR
@@ -22,7 +25,7 @@ graphM<Item>::graphM(const graphM<Item>& source){
     count = source.count;
     matrix = new int*[capacity];
 
-    //Iterate through out the entire matrix. 
+    // Iterate throughout the entire matrix. 
     for (size_t i = 0; i < capacity; i++){
         matrix[i] = new int[capacity](); // Make a new array for each element.
         for(size_t j = 0; j < count;  j++){ // After the array is added, copy over the contents from the source.
@@ -33,46 +36,53 @@ graphM<Item>::graphM(const graphM<Item>& source){
     label = new Item[capacity];
     for (size_t k = 0; k < capacity; k++){
         label[k] = source.label[k]; // Copy over the contents from the source label array.
-    }
-    
+    }   
 }
-//DECONSTRUCTOR?? .. DOESN'T MATTER
+//DECONSTRUCTOR?? ... DOESN'T MATTER IN THIS CASE 
 
 //MODIFICATION MEMBER FUNCTIONS
 template <class Item>
 void graphM<Item>::addVertex(const Item& inLabel){
-    if(count < capacity){
+    if(count < capacity){ // Make sure there is room to add vertex.
     label[count] = inLabel;
     count++;
     }    
 }
 
+// Make sure there is at least 1 vertex.
+// Make sure the target and vertex values are in the matrix.
+// Make sure the weight is NOT a negative number.
 template <class Item>
 void graphM<Item>::addEdge(size_t source, size_t target, int weight){
-    if((source < capacity) && (target < capacity) && (count >= 2) && weight > 0){
+    if((count > 0) && (source < capacity) && (target < capacity) && (weight > 0)){
     	matrix[source][target] = weight;
     }   
 }
 
+// Make sure there is at least 1 vertex.
+// Make sure the target and vertex values are in the matrix.
 template <class Item>
 void graphM<Item>::removeEdge(size_t source, size_t target){
-    if(count >= 2){
+    if((count > 0) && (source < capacity) && (target < capacity)){
     	matrix[source][target] = 0;
     }    
 }
+
 //CONSTANT MEMBER FUNCTIONS
 template <class Item>
 void graphM<Item>::print() const{
     if(count > 0){
-    	for (size_t i = 0; i < capacity; i++){
+        // HORIZONTAL LABELS
+        cout<<setw(2)<<""; // Used to add spacing before the first label.
+    	for (size_t i = 0; i < count; i++){
         	cout<<label[i];
-        	cout<<setw(1)<<"";
+        	cout<<setw(1)<<""; // Add one space after each label.
     	}
     	cout<<"\n";
-   		for (size_t j = 0; j < capacity; j++ ){
-        	cout<<label[j];
-        	cout<<setw(1)<<"";
-        	for (size_t k = 0; k < capacity; k++){
+   		for (size_t j = 0; j < count; j++ ){
+        	cout<<label[j];// Print vertical label.
+        	cout<<setw(1)<<""; // Add space after vertical label.
+        	for (size_t k = 0; k < count; k++){// Print out the matrix. 1 == edge, 0 = no edge.
             	cout<<matrix[j][k];
             	cout<<setw(1)<<"";
         		}
@@ -81,6 +91,7 @@ void graphM<Item>::print() const{
    	}
 }
 
+// Basically return the number of labels which is the number of vertices.
 template <class Item>
 size_t graphM<Item>::numVertices() const{
     return count;
@@ -88,9 +99,10 @@ size_t graphM<Item>::numVertices() const{
 
 template <class Item>
 size_t graphM<Item>::numEdges() const{
-    size_t edgesCount = 0;
-    for(size_t i = 0; i < capacity; i++){
-        for(size_t j = 0; j < capacity; j++){
+    size_t edgesCount = 0; 
+    // Iterate vertically. We stop at count because that is where the vertices exist!
+    for(size_t i = 0; i < count; i++){
+        for(size_t j = 0; j < count; j++){ // Iterate horizontaly.
             if(matrix[i][j] != 0){
                 edgesCount++;
             }
@@ -101,25 +113,23 @@ size_t graphM<Item>::numEdges() const{
 
 template <class Item>
 bool graphM<Item>::isEdge(size_t source, size_t target) const{
-    return (matrix[source][target] != 0);
+    // Check the array at the intersection. Return whether or not there is a weight in the interesction.
+    return (matrix[source][target] > 0);
 }
 
 template <class Item>
 Item* graphM<Item>::neighbors(size_t vertex) const{
     if(count > 0){
-    	Item* track = new Item[count]();
-    	size_t trackSize = 0;
-        	for(size_t i = 0; i < count; i++){
-            	if(matrix[vertex][i] != 0){
-                	track[trackSize] = label[i];
-                	trackSize++;
+    	Item* myNeighbors = new Item[count](); // Make an array to store the possible neighbors.
+    	size_t neighCount= 0;
+        	for(size_t i = 0; i < count; i++){ // Iterate through matrix.
+            	if(matrix[vertex][i] > 0){ // 
+                	myNeighbors[neighCount] = label[i]; // Add the label to the array of neighbors.
+                	neighCount++;
             	}
         	}
-    	Item* myNeighbors = new Item[trackSize];
-    	for(size_t j = 0; j < trackSize; j++){
-        	myNeighbors[j] = track[j];
-    	}
-    delete track;
+    // Take our array and resize it nicely ... functon resize is given in the header file.
+    myNeighbors = resize(myNeighbors,neighCount); // NOTE: can't return resize(myNeighbors,neighCount); because you can't return template types.
     return myNeighbors;
     } 
 }
--- CSC21200/HW6/hw6q2.cpp
+++ CSC21200/HW6/hw6q2.cpp
@@ -6,78 +6,116 @@
 template<class Item>
 graphL<Item>::graphL(){
 	capacity = DEF_CAP;
-	list = new node<graphData>*[capacity];
-	//SET MATRIX
-	for (size_t i = 0; i < capacity; i++){
-		list[i] = NULL;
-	}
-	label = new Item[capacity];
-	count = 0;
+	list = new node<graphData>*[capacity]; // Make an array with node<graphData> pointers.
+		//SET MATRIX
+		for (size_t i = 0; i < capacity; i++){ // Make each pointer in the array point to NULL.
+			list[i] = NULL;
+		}
+	label = new Item[capacity]; // Make an array to hold the vertices.
+	count = 0; // There are no edges so far.
 }
 
-//COPY CONSTRUCTOR
+//COPY CONSTRUCTOR .. NT
 template<class Item>
 graphL<Item>::graphL(const graphL<Item>& source){
+	// Copy over private variables values from source.
+	capacity = source.capacity;
+	count = source.count;
+	label = new Item[capacity];
 
+	list = new node<graphData>*[capacity]; // Initialize Array for list.
+	for (size_t i = 0; i < count; i++){ // Iterate through the source list.
+		if(source.list[i] != NULL){ // If there is an edge for this label.
+			node<graphData>* newHead = NULL;
+			node<graphData>* tail = NULL;
+			node<graphData>* sourcePtr = source.list[i]; // Copy of source head.
+			list_copy(sourcePtr,newHead,tail);
+			list[i] = newHead;
+		}
+		else{
+			continue; // If there isn't an edge for this label aka vertice then move on.
+		}
+	}
 }
 
-//MODODIFICATION MEMBER FUNCTIONS
+//MODIFICATION MEMBER FUNCTIONS
+// Make sure there is room to add another vertice.
 template<class Item>
 void graphL<Item>::addVertex(const Item& inLabel){
-	label[count] = inLabel;	
-	count++;
+	if(count < capacity){ 
+		label[count] = inLabel;	
+		count++;
+	}
 }
 
+// Make sure the vertices index exist in the label array.
 template<class Item>
 void graphL<Item>::addEdge(size_t source, size_t target, int weight){
+	if((source < count ) && (target < count) && (weight > 0)){ 
+	// Make a node containing the vertex it connects to and the weight of this edge we're making.
 	graphData newData(target,weight);
 	node<graphData>* newNode = new node<graphData>(newData);
-	if(list[source] == NULL){
-		list[source] = newNode;
-	}
-	else{
-		node<graphData>*& curr = list[source];
-		while(curr->link() != NULL){
-			if(curr->data().getV() == target){
-			curr->data().setW(weight);
-			}
-			else{
-				curr = curr->link();
-			}	
-		}
-		if(curr->link() == NULL){
-			curr->set_link(newNode);
-		}	
-	}
 
+		// If this vertice doesn't have any edges yet, then make this the first edge.
+		if(list[source] == NULL){
+			list[source] = newNode;
+		}
+		else{
+			// If this vertice already has an edge.
+			node<graphData>*curr = list[source]; // Point to the first edge.
+			node<graphData>*prev = curr; // Make a previous pointer.
+			if(curr->data().getV() == target){ // When the first node has the value of target, then an edge already exists, then change it's weight and leave the function.
+					curr->data().setW(weight);
+					return;
+				}
+			while(curr->link() != NULL){ // When the edge doesn't exist yet. Go to the very last node.
+				curr = curr->link();	
+			}
+			curr->set_link(newNode); // Add the new new node/edge.
+		}
+	}
 }
 
+// Make sure the vertices index exist in the label array.
 template<class Item>
 void graphL<Item>::removeEdge(size_t source, size_t target){
-	if(count == 0){
-		return;
+	if((source < count ) && (target < count)){ 
+		if(list[source] == NULL){ // When the vertice doesn't have an edge, then leave the function.
+			return;
+		}
+		if(list[source]->data().getV() == target){ // If the edge already exists at the first node, then change it's weight and exit the function.
+			list_head_remove(list[source]);
+			return;
+		}
+		node<graphData>* curr = list[source];
+		while(curr->link() != NULL){
+			if(curr->data().getV() == target){ // If we find the edge, then remove the node and leave the function.
+				list_remove(curr);
+				return;
+			}
+			curr = curr->link();
+		}	
 	}
-	node<graphData>* curr = list[source];
-	node<graphData>* prev = curr;
-	while((curr->data().getV() != target) && (curr != NULL)){
-		prev = curr;
-		curr = curr->link();
-	}
-	list_remove(prev);
 }
 
 //CONST MEMBER FUNCTIONS
 template<class Item>
 void graphL<Item>::print() const{
-	for (size_t i = 0; i < count; i++){
-		node<graphData>* curr = list[i];
-		while(curr->link()!= NULL){
-		cout<<label[i]<<"->"<<curr->data().getV();
+	for (size_t j = 0; j < count; j++){// Print label first.
+		node<graphData>* curr = list[j];
+		cout<<label[j]<<"->";
+		while(curr!= NULL){ // Iterate throughout the entire linked list.
+			cout<<curr->data().getV()<<"|"<<curr->data().getW(); // Print out the Vertex | Weight : This is an edge.
+			if(curr->link() != NULL){ // As long as we're not the last node, print out ->
+				cout<<"->";
+			}
+			curr = curr->link();
 		}
-		cout<<"\n";
+		cout<<"\n"; // After we printed out the entire list of edges.
 	}
 }
 
+// Return the number of labels.
 template<class Item>
 size_t graphL<Item>::numVertices() const{
 	return count;
@@ -86,34 +124,47 @@ size_t graphL<Item>::numVertices() const{
 template<class Item>
 size_t graphL<Item>::numEdges() const{
 	size_t edgeCount = 0;
-	for (size_t i = 0; i < count; i++){
-		node<graphData>* curr = list[i];
-		while(curr!= NULL){
-			edgeCount++;
-			curr = curr->link();
+	for(size_t i = 0 ; i < count; i++){
+		node<graphData>* curr = list[i]; // Point to the first index of the list.
+		if(curr == NULL){ // If the index doesn't point to a node.
+			if(i != count-1){ // If this index isn't the last index, go on to the next pointer in the list array.
+				continue;
+			}	
+		}
+		else{
+			while(curr != NULL){ // Let's iterate through the entire linked list, and keep track of the number of edges.
+				edgeCount++;
+				curr = curr->link();
+			}
 		}
 	}
 	return edgeCount;
 }
 
+// Make sure the index of the label array exists, this means that the vertices exist.
 template<class Item>
 bool graphL<Item>::isEdge(size_t source, size_t target) const{
 	if(count > 0){
-	node<graphData> *curr = list[source];
-		while(curr != NULL){
-			if((curr->data() == target)){
+	node<graphData> *curr = list[source]; 
+		while(curr != NULL){ // Iterate through the entire linked list.
+			if(curr->data().getV() == target){ // If we found an edge, then return true.
 				return true;
 			}
 			curr = curr->link();
 		}	
-	return false;
+	return false; // We iterated throughout the entire linked list, the edge connected to the target hasn't been found.
 	}
-	
 }
 
+// Make sure the vertice has any edges.
 template<class Item>
 node<Item>* graphL<Item>::neighbors(size_t vertex) const{
-
+	node<graphData>* newHead; // Head of Linked list of neighbors
+	if(list[vertex] != NULL){
+		node<graphData>* startNode = list[vertex]; // Source pointer.
+		node<graphData>* tail;
+		list_copy(startNode,newHead,tail);	
+	}
+	return newHead;
 }
-
 #endif
