81ddf68b3497470299477c47837690cb1dc7ad2d
hpuma <pumahenry@live.com>
2019-05-13 15:24:22 -0400

    began_making_test_files

---
+++ CSC21200_PRACTICE/bagDynamic/test/test.cpp
@@ -0,0 +1,27 @@
+#include "bagDynamic.h"
+#include "gtest/gtest.h"
+
+TEST(dynamic, constructor) {
+	bag myBag;
+	EXPECT_EQ(true, myBag.isEmpty());
+	EXPECT_EQ(0, myBag.get_size());
+	EXPECT_EQ(30, myBag.get_capacity());
+}
+
+TEST(dynamic, addVertex) {
+	bag myBag;
+
+	myBag.insert(8);
+	myBag.insert(9);
+	myBag.insert(10);
+	myBag.insert(11);
+	EXPECT_EQ(false, myBag.isEmpty());
+	EXPECT_EQ(4, myBag.get_size());
+	EXPECT_EQ(30, myBag.get_capacity());
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest( &argc, argv );
+    return RUN_ALL_TESTS();
+}
