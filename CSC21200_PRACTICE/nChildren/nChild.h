d3fbc7b96dbcfba38f0b4dc9e5ed6f89a2cc0441
Henry_Puma <pumahenry@live.com>
2019-05-14 12:35:40 -0400

    nChild_edit

--- CSC21200_PRACTICE/nChildren/nChild.h
+++ CSC21200_PRACTICE/nChildren/nChild.h
@@ -1,8 +1,9 @@
 #ifndef N_CHILD_NODE_H
 #define N_CHILD_NODE_H
 
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
-
 using namespace std;
 
 template<class Item>
@@ -11,27 +12,27 @@ public:
 	const size_t DEF_CAP=30;
 	// CONSTRUCTOR
 	nChild(const Item& initData=Item()){
-		children = new nChild<Item>*[DEF_CAP];
+		children = new nChild<Item>*[DEF_CAP]; // Make Dyanimic array with pointers to nChild<Item> node.
 		capacity = DEF_CAP;
 		data = initData;
 		count = 0;
-		for(size_t i = 0; i < capacity; i++){
+		for(size_t i = 0; i < capacity; i++){ // Set all the nChild<Item> pointers to NULL.
 			children[i] = NULL;
 		}
 	}
 	// CONSTANT MEMBER FUNCTIONS.
-	Item getData() const{ return data;}
-	nChild<Item>** getChildren() const{ return children;} // Returns Array containing pointers to children.
-	bool isEmpty() const{ return (count == 0);}
-	size_t size() const{ return count;}
+	Item getData() const{ return data;} // Get the value of the node
+	nChild<Item>** getChildren() const{ return children;} // Returns array containing pointers to children.
+	bool isEmpty() const{ return (count == 0);} // Checks whether or not the array of children are empty.
+	size_t size() const{ return count;}  // Returns the number of chuldren that this node has.
 
 	// MODIFICATION MEMBER FUNCTIONS.
-	void setData(Item newData){data = newData;}
-	void addChild(nChild<Item>* newChild){
+	void setData(Item newData){data = newData;} // Changes the data of this node.
+	void addChild(nChild<Item>* newChild){ // Adds a node to the list of this node's children.
 		children[count] = newChild;
 		count++;
 	}
-	nChild<Item>* getChild(size_t i){
+	nChild<Item>* getChild(size_t i){ // Gets the children at a specific position in the array of children.
 		if((i >= 0) && (i < count)){
 			return children[i];
 		}
