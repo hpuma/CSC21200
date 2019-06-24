233690a7af7061cab34ac5727b06aeb1447b26ac
Henry_Puma <pumahenry@live.com>
2019-06-24 00:01:57 -0400

    few_comments

--- HW4/hw4q2.cpp
+++ HW4/hw4q2.cpp
@@ -19,23 +19,23 @@ first = source.first;
 last = source.last;
 used = source.used;
 
-Item* newData = new Item[source.capacity];
-    for(size_t i = 0; i < source.used; i++){
+Item* newData = new Item[source.capacity]; // Create new dynamic array the same size as the Source dynamic array.
+    for(size_t i = 0; i < source.used; i++){ // Add everything from this dynamic array to the new dynamic array.
         newData[i] = source.data[i];
     }
-data = newData;
+data = newData; // Set the data pointer to the new dynamic array with the new data.
 
 }
 
 template <class Item>
 Queue<Item>::~Queue(){
-delete []data;
+delete []data; // Delete the data pointer.
 }
 
 template <class Item>
 void Queue<Item>::push(const Item& entry){
-    if(!(used > capacity)){
-        last = nextIndex(last);
+    if(!(used > capacity)){ // Check if there is enough space to add another element.
+        last = nextIndex(last); // Next index is private function that determines the next index within the array.
         data[last] = entry;
         ++used;   
     }
@@ -43,7 +43,7 @@ void Queue<Item>::push(const Item& entry){
 }
 template <class Item>
 void Queue<Item>::pop(){
-    if(!(empty())){
+    if(!(empty())){ // Make sure we are not popping an empty stack.
         first = nextIndex(first);
         --used;
     }
@@ -56,27 +56,27 @@ void Queue<Item>::operator=(const Queue<Item>& source){
     last = source.last;
     used = source.used;
 
-    Item* newData = new Item[source.capacity];
-    for(size_t i = 0; i < source.used; i++){
+    Item* newData = new Item[source.capacity]; // Allocate new data array.
+    for(size_t i = 0; i < source.used; i++){ // Move contents over.
         newData[i] = source.data[i];
     }
-    data = newData;
+    data = newData; // Point to the newly allocated array.
 }
 
 template <class Item>
 size_t Queue<Item>::size() const{
-    return used;
+    return used; // Return the number of used indexes.
 }
 template <class Item>
 bool Queue<Item>::empty() const{
-    return (used == 0);
+    return (used == 0); // Return whether or not the number of used elements is not 0.
 }
 
 template <class Item>
 Item Queue<Item>::front() const{
-    if(!(empty())){
+    if(!(empty())){ // Make sure the data array is not empty!
         return data[first];
     }
-    return Item();
+    return Item(); // Return a garbage Value.
 }
 #endif
