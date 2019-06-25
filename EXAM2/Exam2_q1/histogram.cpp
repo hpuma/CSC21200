8c884c8c09ac73a2ed02c7b3a6cad377cfbc9ce1
Henry_Puma <pumahenry@live.com>
2019-06-25 15:47:59 -0400

    added_comments_for_clarification

--- EXAM2/Exam2_q1/histogram.cpp
+++ EXAM2/Exam2_q1/histogram.cpp
@@ -6,7 +6,11 @@
 #include<cstdio>
 #include<cstdlib>
 using namespace std;
+// Function that takes an array and turns it into a linked list.
 node<int>* array_to_list(int inputArray[],size_t arraySize){
+	if(arraySize == 0){
+		return NULL;
+	}
 	node<int>* head = new node<int>(inputArray[0]);
 	node<int>* builder = head;
 	node<int>* sentinel = NULL;
@@ -20,19 +24,19 @@ node<int>* array_to_list(int inputArray[],size_t arraySize){
 
 
 node<hist>* histogram(node<int>* head){
-	if(head == NULL){ // When we're given a linked list.
+	if(head == NULL){ // When the head pointer is pointing to NULL.
 		cout<<"HEAD IS NULL\n";
 		return NULL;
 	}
-	size_t arraySize = list_length(head);
-	int* nodeValue = new int[arraySize]; // Make a new array the size of the linked list.
-	node<int>* ptr = head;
-	// Adding everything to the array.
+	size_t arraySize = list_length(head); // Get the length of the current linked list.
+	int* nodeValue = new int[arraySize]; // Make a new array that is the size of the linked list
+	node<int>* ptr = head; // Set a pointer to the head of the linked list.
+	// Adding everything  from the linked list to the array.
 	for(size_t i = 0; i < arraySize; i++){
 		nodeValue[i] = ptr->data();
 		ptr = ptr->link();
 	}
-	// Sorting the array.
+	// Sorting the array. All the duplicates will be right next to each other.
 	for(size_t j = 0; j < arraySize; j++){
 		for(size_t k = j+1; k < arraySize; k++){
 			if(nodeValue[k] < nodeValue[j]){
@@ -42,23 +46,23 @@ node<hist>* histogram(node<int>* head){
 			}
  		}
 	}
-	hist headVal(nodeValue[0]);
-	node<hist>* histHead = new node<hist>(headVal);
-	node<hist>* builder = histHead;
-	node<hist>* sentinel  = NULL;
-
+	hist headVal(nodeValue[0]); // Makes a hist item that contains the first value of the array.
+	node<hist>* histHead = new node<hist>(headVal); // Set the node<hist> head pointer to a new node containing headVal.
+	node<hist>* builder = histHead; // Set a builder pointer to the head of the linked list.
+	node<hist>* sentinel  = NULL; // Making a dummy node that will be used to make a new node.
+	// We begin at 1 because we already took the first value of the array already.
 	for(size_t l = 1; l < arraySize; l++){
-		if(nodeValue[l] != nodeValue[l-1]){
-			hist histMake(nodeValue[l]);
-			sentinel = new node<hist>(histMake);
-			builder->set_link(sentinel);
-			builder = sentinel;
+		if(nodeValue[l] != nodeValue[l-1]){ // When the current value doesn't equal the previous value.
+			hist histMake(nodeValue[l]); // Make a hist of the uniq value, the count is already set to 1.
+			sentinel = new node<hist>(histMake); // Make a node containing the hist item from above.
+			builder->set_link(sentinel); // Make the "builder" pointer set the link to the dummy node "sentinel".
+			builder = builder->link(); // A new node ahead has been created, lets go to it.
 		}
-		else{
-			builder->data().increaseCt();
+		else{ // When the current value equals the previous value.
+			builder->data().increaseCt(); // The value has repeated itself... increment the value within the linked list
 		}
 	}
-	return histHead;
+	return histHead; // Return the head of the list.
 }
 
 
